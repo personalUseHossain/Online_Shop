d6dce08c8bc64214c4034334c8568ee9decd9d50
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-07-02 18:51:23 +0600

    diffrent route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 
+//router
+import { Route, Routes } from "react-router-dom";
+
 // importing universal css
 import "./CSS/Universal.css";
 
 // importing all components
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Footer from "./Components/Footer";
+import Cart from "./Components/Cart";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Home /> */}
+      <Routes>
+        <Route path="/" Component={Home}></Route>
+        <Route path="/cart" Component={Cart}></Route>
+        <Route path="/login" Component={Login}></Route>
+        <Route path="/signup" Component={Signup}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
