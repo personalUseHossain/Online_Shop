cac0117502c87573ea64d24bdbbd00536012e4d1
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-07-04 22:33:15 +0600

    input validation added (not complete)

--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 //css
 import "../CSS/Signup.css";
@@ -15,6 +16,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Signup() {
+  const navigate = useNavigate(); //navigate from react-router-dom to send user to login page
+
   //input useState
   const [allInput, setAllInput] = useState({
     name: "",
@@ -31,57 +34,72 @@ export default function Signup() {
   }
 
   //submitting data to database
-  async function handleSumbit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    const req = await axios.post("http://localhost:5000/signup", allInput);
-    const data = req.data;
-    console.log(data);
+    axios
+      .post("http://localhost:5000/signup", allInput)
+      .then((res) => {
+        alert("Sucessfully user created");
+        navigate("/login");
+      })
+      .catch((err) => console.log(err));
   }
-
   return (
     <div className="signup">
-      <form onSubmit={(e) => handleSumbit(e)}>
+      <form method="POST">
         <label>
-          <FontAwesomeIcon className="signup-icon" icon={faUser} />
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            onChange={(e) => handleInput(e)}
-            value={allInput.name}
-          />
+          <div className="input">
+            <FontAwesomeIcon className="signup-icon" icon={faUser} />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={(e) => handleInput(e)}
+              value={allInput.name}
+            />
+          </div>
+          <small>Name is required</small>
         </label>
         <label>
-          <FontAwesomeIcon className="signup-icon" icon={faEnvelope} />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            onChange={(e) => handleInput(e)}
-            value={allInput.email}
-          />
+          <div className="input">
+            <FontAwesomeIcon className="signup-icon" icon={faEnvelope} />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={(e) => handleInput(e)}
+              value={allInput.email}
+            />
+          </div>
+          <small className="emailErr">Email is required</small>
         </label>
         <label>
-          <FontAwesomeIcon className="signup-icon" icon={faLock} />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={(e) => handleInput(e)}
-            value={allInput.password}
-          />
+          <div className="input">
+            <FontAwesomeIcon className="signup-icon" icon={faLock} />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={(e) => handleInput(e)}
+              value={allInput.password}
+            />
+          </div>
+          <small>Password is required</small>
         </label>
         <label>
-          <FontAwesomeIcon className="signup-icon" icon={faKey} />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            onChange={(e) => handleInput(e)}
-            value={allInput.confirmPassword}
-          />
+          <div className="input">
+            <FontAwesomeIcon className="signup-icon" icon={faKey} />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              onChange={(e) => handleInput(e)}
+              value={allInput.confirmPassword}
+            />
+          </div>
+          <small className="confirmPassErr">Confirm Password is required</small>
         </label>
-        <button>SIGN UP</button>
+        <button onClick={handleSubmit}>SIGN UP</button>
       </form>
       <p>
         Have A Account?
