e509853b7e523ae8462de341e2729e8f573e5915
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-07-04 22:40:10 +0600

    more context added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-
-//router
-import { Route, Routes } from "react-router-dom";
-
-// importing universal css
-import "./CSS/Universal.css";
+import React, { createContext, useContext, useState } from "react";
+import { Route, Routes } from "react-router-dom"; //router
+import "./CSS/Universal.css"; // importing universal css
+import Cookies from "universal-cookie";
 
 // importing all components
 import Navbar from "./Components/Navbar";
@@ -14,17 +11,35 @@ import Cart from "./Components/Cart";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 
+export const Context = createContext();
 function App() {
+  const cookies = new Cookies();
+  const [isLogin, setIsLogin] = useState(
+    cookies.get("login_token") === undefined ? false : true
+  ); //for check if user is logged in or not
+  const [userData, setUserData] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(cookies.get("isAdmin") || false);
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" Component={Home}></Route>
-        <Route path="/cart" Component={Cart}></Route>
-        <Route path="/login" Component={Login}></Route>
-        <Route path="/signup" Component={Signup}></Route>
-      </Routes>
-      <Footer />
+      <Context.Provider
+        value={{
+          isLogin,
+          setIsLogin,
+          userData,
+          setUserData,
+          isAdmin,
+          setIsAdmin,
+        }}
+      >
+        <Navbar />
+        <Routes>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/cart" Component={Cart}></Route>
+          <Route path="/login" Component={Login}></Route>
+          <Route path="/signup" Component={Signup}></Route>
+        </Routes>
+        <Footer />
+      </Context.Provider>
     </>
   );
 }
