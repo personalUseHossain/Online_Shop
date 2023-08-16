ae493d89693a865fcafec2bb20f0904fc7e1ce5a
Muhammad Hossain <personal.mdhossain@gmail.com>
2023-08-16 11:14:58 +0600

    new component added and all cookies together

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,34 +10,42 @@ import Footer from "./Components/Footer";
 import Cart from "./Components/Cart";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import UpdateUserInfo from "./Components/UpdateUserInfo";
+import Dashboard from "./Components/Admin/Dashboard";
+import InsertProduct from "./Components/Admin/InsertProduct";
 
 export const Context = createContext();
 function App() {
   const cookies = new Cookies();
-  const [isLogin, setIsLogin] = useState(
-    cookies.get("login_token") === undefined ? false : true
-  ); //for check if user is logged in or not
-  const [userData, setUserData] = useState([]);
-  const [isAdmin, setIsAdmin] = useState(cookies.get("isAdmin") || false);
+  // const [isLogin, setIsLogin] = useState(
+  //   cookies.get("login_token") === undefined ? false : true
+  // ); //for check if user is logged in or not
+  const [online_shop_cookies, set_online_shop_cookies] = useState(
+    cookies.get("online_shop_cookies") || {}
+  );
+  console.log(online_shop_cookies);
+  // const [userData, setUserData] = online_shop_cookies.jwt || [];
+  // const [isAdmin, setIsAdmin] = online_shop_cookies.isAdmin || false;
   return (
     <>
       <Context.Provider
         value={{
-          isLogin,
-          setIsLogin,
-          userData,
-          setUserData,
-          isAdmin,
-          setIsAdmin,
+          online_shop_cookies,
+          set_online_shop_cookies,
         }}
       >
         <Navbar />
-        <Routes>
-          <Route path="/" Component={Home}></Route>
-          <Route path="/cart" Component={Cart}></Route>
-          <Route path="/login" Component={Login}></Route>
-          <Route path="/signup" Component={Signup}></Route>
-        </Routes>
+        <div className="main">
+          <Routes>
+            <Route path="/" Component={Home}></Route>
+            <Route path="/cart" Component={Cart}></Route>
+            <Route path="/login" Component={Login}></Route>
+            <Route path="/signup" Component={Signup}></Route>
+            <Route path="/update/userinfo" Component={UpdateUserInfo}></Route>
+            <Route path="/dashboard" Component={Dashboard}></Route>
+            <Route path="/insert/product" Component={InsertProduct}></Route>
+          </Routes>
+        </div>
         <Footer />
       </Context.Provider>
     </>
